2c0cb66a93fd2d3a155be3afd5cc9fdf84609bb2
Hosamov <hosamov@hotmail.com>
2022-06-23 08:30:46 -0700

    add basic camera movement

--- main.cpp
+++ main.cpp
@@ -11,7 +11,10 @@ int main() {
   InitWindow(windowDimensions[0], windowDimensions[1], "Classy Clash");
 
   // Map variables
-  Texture2D map = LoadTexture("nature_tileset/worldmap.png");
+  Texture2D map = LoadTexture("nature_tileset/worldmap.png");\
+  Vector2 mapPos{0.0, 0.0};
+
+  float speed{4.0};
 
   SetTargetFPS(60);
   while(!WindowShouldClose()) { 
@@ -19,8 +22,18 @@ int main() {
     BeginDrawing();
     ClearBackground(WHITE);
 
-    // Draw the background
-    Vector2 mapPos{0.0, 0.0};
+    // Direction logic
+    Vector2 direction{};
+    if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+    if (IsKeyDown(KEY_D)) direction.x += 1.0;
+    if (IsKeyDown(KEY_W)) direction.y -= 1.0; // Up direction is negative val
+    if (IsKeyDown(KEY_S)) direction.y += 1.0;
+    if (Vector2Length(direction) != 0.0) {
+      // Set mapPos = mapPos - direction
+      mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+    }
+
+    // Draw the map
     DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
     // End drawing
