3630d144161b46fdebb1fa207a4bbf7868d23761
Hosamov <hosamov@hotmail.com>
2022-07-05 11:30:38 -0700

    update enemy

--- main.cpp
+++ main.cpp
@@ -1,24 +1,8 @@
 #include "raylib.h"
 #include "raymath.h"
-
-class Character {
-  public:
-    Vector2 getWorldPos() { return worldPos; }
-
-  private:
-    Texture2D texture;
-    Texture2D idle;
-    Texture2D run;
-    Vector2 screenPos;
-    Vector2 worldPos;
-    // 1 : facing right, -1 : facing left
-    float rightLeft{1.f};
-    // animation variables
-    float runningTime{};
-    int frame{};
-    const int maxFrames{6};
-    const float updateTime{1.f/12.f};
-};
+#include "Character.h"
+#include "Prop.h"
+#include "Enemy.h"
 
 int main() {
   // Window dimensions
@@ -32,22 +16,23 @@ int main() {
   // Map variables
   Texture2D map = LoadTexture("nature_tileset/worldmap.png");\
   Vector2 mapPos{0.0, 0.0};
-  float speed{4.0};
+  const float mapScale{4.0f};
 
-  Texture2D knight_idle = LoadTexture("characters/knight_idle_spritesheet.png");
-  Texture2D knight_run = LoadTexture("characters/knight_run_spritesheet.png");
-  Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
-  Vector2 knightPos{
-    (float)windowDimensions[0]/2.0f - 4.0f * (0.5f * (float)knight.width/6.0f),
-    (float)windowDimensions[1]/2.0f - 4.0f * (0.5f * (float)knight.height)
+  Character knight{windowDimensions[0], windowDimensions[1]};
+
+  // Draw props
+  Prop props[2]{
+    Prop{Vector2{600.f, 300.f}, LoadTexture("nature_tileset/Rock.png")},
+    Prop{Vector2{400.f, 500.f}, LoadTexture("nature_tileset/Log.png")}
   };
-  // 1 : facing right, -1 : facing left
-  float rightLeft{1.f};
-  // animation variables
-  float runningTime{};
-  int frame{};
-  const int maxFrames{6};
-  const float updateTime{1.f/12.f};
+
+  Enemy goblin{
+    Vector2{0.f, 0.f}, 
+    LoadTexture("characters/goblin_idle_spritesheet.png"), 
+    LoadTexture("characters/goblin_run_spritesheet.png")
+  };
+
+  goblin.setTarget(&knight);
 
   SetTargetFPS(60);
   while(!WindowShouldClose()) { 
@@ -55,36 +40,34 @@ int main() {
     BeginDrawing();
     ClearBackground(WHITE);
 
-    // Direction logic
-    Vector2 direction{};
-    if (IsKeyDown(KEY_A)) direction.x -= 1.0;
-    if (IsKeyDown(KEY_D)) direction.x += 1.0;
-    if (IsKeyDown(KEY_W)) direction.y -= 1.0; // Up direction is negative val
-    if (IsKeyDown(KEY_S)) direction.y += 1.0;
-    if (Vector2Length(direction) != 0.0) {
-      // Set mapPos = mapPos - direction
-      mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
-      direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f; // Ternary operator
-      knight = knight_run;
-    } else {
-      knight = knight_idle;
-    }
+    mapPos = Vector2Scale(knight.getWorldPos(), -1.f);
 
     // Draw the map
-    DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
-
-    // update animation frame
-    runningTime += GetFrameTime(); // delta time
-    if (runningTime >= updateTime) {
-      frame++;
-      runningTime = 0.f;
-      if (frame > maxFrames) frame = 0;
+    DrawTextureEx(map, mapPos, 0.0, mapScale, WHITE);
+    
+    // Draw the props
+    for(auto prop : props) {
+      prop.Render(knight.getWorldPos());
     }
 
-    // Draw the character
-    Rectangle source{frame * (float)knight.width/6.f, 0.f, rightLeft * (float)knight.width/6.f, (float)knight.height};
-    Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
-    DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
+    knight.tick(GetFrameTime());
+    // Check map bounds
+    if(knight.getWorldPos().x < 0.f || 
+       knight.getWorldPos().y < 0.f ||
+       knight.getWorldPos().x + windowDimensions[0] > map.width * mapScale ||
+       knight.getWorldPos().y + windowDimensions[1] > map.height * mapScale ) 
+    {
+      knight.undoMovement();
+    }
+
+    // Check prop collisions
+    for(auto prop: props) {
+      if(CheckCollisionRecs(prop.getCollisionRec(knight.getWorldPos()), knight.getCollisionRec())) {
+        knight.undoMovement();
+      }
+    }
+
+    goblin.tick(GetFrameTime());
 
     // End drawing
     EndDrawing();
